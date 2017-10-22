1c17dd0143185651af1f85a2a561d44dc90e67c5
Riccardo <riccardocalligaro@gmail.com>
2017-10-22 14:18:47 +0200

    Versione 1.1
    Pulito il codice, rimosso codice non necessarie aggiunti molti commenti.

--- js/main.js
+++ js/main.js
@@ -1,127 +1,89 @@
-//aggiunge classe a compasso se la grandezza dello schermo e' minore di 1350
-jQuery(document).ready(function ($) {
-
-  $('#checkbox').change(function(){
-    setInterval(function () {
-        moveRight();
-    }, 3000);
-  });
-
-	var slideCount = $('#slider ul li').length;
-	var slideWidth = $('#slider ul li').width();
-	var slideHeight = $('#slider ul li').height();
-	var sliderUlWidth = slideCount * slideWidth;
-
-	$('#slider').css({ width: slideWidth, height: slideHeight });
-
-	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-
-    $('#slider ul li:last-child').prependTo('#slider ul');
-
-    function moveLeft() {
-        $('#slider ul').animate({
-            left: + slideWidth
-        }, 200, function () {
-            $('#slider ul li:last-child').prependTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
-
-    function moveRight() {
-        $('#slider ul').animate({
-            left: - slideWidth
-        }, 200, function () {
-            $('#slider ul li:first-child').appendTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
-
-    $('a.control_prev').click(function () {
-        moveLeft();
-    });
-
-    $('a.control_next').click(function () {
-        moveRight();
-    });
-
-});
-
-
+// funzione eseguita quando si preme il pulsante invia nome,
   $('#invia_nome').click(function(){
-    var a = $("#nome").val();
-
-      if (a == "") {
+    // salva nella variabile nome il nome
+    var nome = $("#nome").val();
+    //se non si inserisce nessun nome allora si viene chiamati signor nessuno
+      if (nome == "") {
       $('#mostra').text("ciao signor nessuno!");
       $("#invia_nome").addClass('animated shake');
       $("#invia_nome").addClass('button_border');
       $("#invia_nome").off()
-
       }
+
+      // se il nome viene inserito allora si scrive sulla textbot mostra aggiungendo un punto esclamativo e faccina
       else {
-        $('#mostra').append(a);
+        $('#mostra').append(nome);
         $('#mostra').append("! 😊");
         $("#invia_nome").off()
       }
-
+//avvia un timer da 1 s
       setTimeout(
   function()
   {
+        //gli elementi spariscono con una transiziones
           $('#mostra').fadeOut(1000)
           $("#invia_nome").fadeOut(1000)
           $("#nome").fadeOut(1000)
 
   }, 1000);
 
-
+//avvia un timer da 2s
   setTimeout(
 function()
 {
-      $('#anni_testo').append(a);
+      $('#anni_testo').append(nome);
+      //setta il nome e compaiono gli elementi della domanda succesiva
       $('#anni_testo').append("?");
       $('#anni').fadeIn(1500)
       $('#anni_testo').fadeIn(1500)
       $('#invia_anni').fadeIn(1500)
 }, 2000);
 
-
   });
 
-  $("#invia_anni").click(function() {
 
+// fine dello javascrit che chiede il nome
+
+//inizo dello js che chiede gli anni
+
+  $("#invia_anni").click(function() {
+        //salva nella variabile anni il testo inserito
         var anni = $("#anni").val();
 
+        //se il testo non contiene niente
          if (anni == "") {
-
+           // fa una battuta
              $('#anni_testo').text("Non sei ancora nato?! 😂");
+          //aggiunta animazione
              $("#invia_anni").addClass('animated shake');
              $("#invia_anni").addClass('button_border');
+           //quando finisce l'animazione
              $('#invia_anni').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
             $("#invia_anni").removeClass('animated shake');
              });
-
           }
 
-
+          // se il testo e' uguale a qualcosa allora
           else {
-
-            if (anni>=2 && anni<4) {
-
-              $("#anni_testo").fadeOut(500)
+            // dai due ai 4 anni
+            if (anni==2) {
+              //transazione che fa scomparire i vari input e testi
               $("#invia_anni").fadeOut(500)
               $("#anni").fadeOut(500)
-
+              //timer che fa andare via gli elementi
               setTimeout(
             function()
             {
-              $("#anni_testo").hide()
               $("#invia_anni").hide()
               $("#anni").hide()
             }, 1000);
             setTimeout(
             function()
             {
+              //mostra il gioco adatto
+              $("#anni_testo").text("Scopri moltissimi giochi divertenti per la tua fascia d'eta' qui!")
               $("#clementino_2").fadeIn(1000);
-            }, 500);
+            }, 1000);
 
             }
 
@@ -145,8 +107,8 @@ function()
 
             }
 
-            if (anni>="14") {
-
+            if (anni=="14") {
+              //se anni == 14
               $("#invia_anni").fadeOut(500)
               $("#anni").fadeOut(500)
               setTimeout(
@@ -164,10 +126,9 @@ function()
               $("#14anni").fadeIn(1000);
             }, 500);
 
-
-
             }
 
+//se si inserisce data fasulla
             if (anni>100) {
               $("#anni_testo").text("Sei troppo vecchio! 😂")
               $("#invia_anni").addClass('animated shake');
@@ -176,37 +137,76 @@ function()
              $("#invia_anni").removeClass('animated shake');
               });
             }
-
-
-
-
+//se non sono presenti giochi adatti
+            if (anni > 20) {
+              $("#anni_testo").text("Non abbiamo giochi adatti per te! 😞")
+              $("#invia_anni").addClass('animated shake');
+              $("#invia_anni").addClass('button_border');
+              $('#invia_anni').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+             $("#invia_anni").removeClass('animated shake');
+              });
+            }
           }
-
   });
 
+//fine del quiz
+
+
+
+// INIZIO NAVBAR
+
+
+// animazioni della nav bar
+
+
+//apertura navbar
+$('#nav_bar_mobile_button').click(function(){
+
+ $("#bottone_mobile").hide()
+ $("#bottone_mobile").removeClass("mobile_open")
+ $("#bottone_mobile").addClass("mobile_close")
+
+ $(".navbar_mobile").show()
+ $(".navbar_mobile").addClass('animated bounceInUp')
+ $(".body_centrale").addClass("display_nothing")
+ $("footer").addClass("display_nothing")
+ $("#invia_nome").addClass("display_nothing")
+ $("#mostra").addClass("display_nothing")
+ $("#nome").addClass("display_nothing")
+ $("header").addClass("header_mobile")
+ $("#invia_anni").addClass("display_nothing")
+ $("#close_icon").show()
+ $("#close_icon").addClass("animated bounceInDown")
+ $(".testo").addClass("display_nothing")
+
+
+});
+
+
+//animazione di quando si preme il bottone per chiudere la navbar da mobile
 $("#close_icon").click(function() {
 $(".testo").removeClass("display_nothing")
-   $("#invia_anni").removeClass("display_nothing")
+$("#invia_anni").removeClass("display_nothing")
 $(".body_centrale").removeClass("display_nothing")
- $(".body_centrale").addClass('animated bounceInUp')
- $("#bottone_mobile").show()
+$(".body_centrale").addClass('animated bounceInUp')
+$("#bottone_mobile").show()
 $("#bottone_mobile style").remove()
 $("#bottone_mobile").addClass("mobile_open")
- $("#bottone_mobile").addClass('animated bounceInUp')
- $("#bottone_mobile").css("style", "")
- $(".navbar_mobile").hide()
+$("#bottone_mobile").addClass('animated bounceInUp')
+$("#bottone_mobile").css("style", "")
+$(".navbar_mobile").hide()
 $(".navbar_mobile").removeClass('animated bounce')
 $("footer").removeClass("display_nothing")
- $("footer").addClass('animated bounceInUp')
+$("footer").addClass('animated bounceInUp')
 $("#invia_nome").removeClass("display_nothing")
- $("#invia_nome").addClass('animated bounceInUp')
+$("#invia_nome").addClass('animated bounceInUp')
 $("#mostra").removeClass("display_nothing")
- $("#mostra").addClass('animated bounceInUp')
+$("#mostra").addClass('animated bounceInUp')
 $("#nome").removeClass("display_nothing")
- $("#nome").addClass('animated bounceInUp')
+$("#nome").addClass('animated bounceInUp')
 $("header").removeClass("header_mobile")
- $("header").addClass('animated bounceInUp')
- $(".testo").addClass("animated bounceInUp")
+$("header").addClass('animated bounceInUp')
+$(".testo").addClass("animated bounceInUp")
 $('.button_mobile').removeAttr('id','button_close');
 $("#close_icon").hide()
 $("#nav_bar_mobile_button").removeClass("display_nothing")
@@ -214,27 +214,3 @@ $('#nav_bar_mobile_button').fadeIn(1000)
 
 
 });
-
-  //funzione navbar da mobile
-  //apertura navbar
-  $('#nav_bar_mobile_button').click(function(){
-
-   $("#bottone_mobile").hide()
-   $("#bottone_mobile").removeClass("mobile_open")
-   $("#bottone_mobile").addClass("mobile_close")
-
-   $(".navbar_mobile").show()
-   $(".navbar_mobile").addClass('animated bounceInUp')
-   $(".body_centrale").addClass("display_nothing")
-   $("footer").addClass("display_nothing")
-   $("#invia_nome").addClass("display_nothing")
-   $("#mostra").addClass("display_nothing")
-   $("#nome").addClass("display_nothing")
-   $("header").addClass("header_mobile")
-   $("#invia_anni").addClass("display_nothing")
-   $("#close_icon").show()
-   $("#close_icon").addClass("animated bounceInDown")
-   $(".testo").addClass("display_nothing")
-
-
-  });
