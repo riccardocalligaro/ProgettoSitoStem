87aac0f10240c7fde45e5bc07b33385afaa3998f
Riccardo <riccardocalligaro@gmail.com>
2017-11-03 08:48:23 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Vede se si tratta di un dispositivo mobile
+// script che rileva se si tratta di un duispositivo mobile, crea una variabile che risulterà vera se è un telefono
  var isMobile = {
      Android: function() {
          return navigator.userAgent.match(/Android/i);
@@ -19,65 +19,15 @@
          return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
      }
  };
-// quiz della materia
-  $("#invia_materia").click(function() {
-    var materia = $("#materia").val().toLowerCase()
-
-    if (materia == "matematica") {
-      var battue_matematica = new Array("Chi non conosce la matematica difficilmente riesce a cogliere la bellezza, la più intima bellezza, della natura.",
-                                        "Sono persuaso che la matematica sia il più importante strumento di conoscenza fra quelli lasciatici in eredità dall’agire umano, essendo la fonte di tutte le cose.",
-                                        "Come si dice bellezza poetica si dovrebbe dire altresì dire bellezza matematica. -Blaise Pascal" )
-        battuta_scelta_matematica = battue_matematica[Math.floor( Math.random() * battue_matematica.length )];
-
-      $('#materia_testo').text(battuta_scelta_matematica);
 
 
-
-    }
-
-    if (materia == "ingenieria") {
-      var ingenieria_batutte = new Array("La scienza è sapere; l’ingegneria è fare.",
-                                        "Saranno anche i guerrieri a conquistare la gloria, ma sono gli ingegneri che costruiscono la società.",
-                                        "Una volta sono riuscito a comprendere cosa diceva un ingegnere, ma poi per una settimana non capivo più cosa diceva la gente normale.",
-                                        "Lo scienziato descrive ciò che esiste; l’ingegnere crea ciò che non era mai stato.",
-
-                                       )
-        battuta_scelta_ingenieria = ingenieria_batutte[Math.floor( Math.random() * ingenieria_batutte.length )];
-      $('#materia_testo').text(battuta_scelta_ingenieria);
-    }
+// Esistono 3 variabili
+// Nome = nome persona
+// anni = anni persona
+// materia = materia preferita
 
 
-    if (materia == "scienza") {
-      var scienza_batutte = new Array("La scienza non è che la spiegazione di un miracolo che non riusciamo mai a spiegare e l’arte è un’interpretazione di quel miracolo..",
-                                        "La Natura compone alcune delle sue poesie più belle davanti al microscopio e al telescopio.",
-                                        "La più bella e profonda emozione che possiamo provare è il senso del mistero; sta qui il seme di ogni arte, di ogni vera scienza.",
-                                        "Niente nella vita va temuto, dev’essere solamente compreso. Ora è tempo di comprendere di più, così possiamo temere di meno.",
-
-                                       )
-        battuta_scelta_scienza = scienza_batutte[Math.floor( Math.random() * scienza_batutte.length )];
-      $('#materia_testo').text(battuta_scelta_scienza);
-    }
-
-    if (materia == "tecnologia") {
-      var tecnologia_batutte = new Array("I computer sono incredibilmente veloci, accurati e stupidi. Gli uomini sono incredibilmente lenti, inaccurati e intelligenti. L'insieme dei due costituisce una forza incalcolabile. ",
-                                        "Il computer è la bicicletta della nostra mente. -Steve Jobs.",
-                                        "640 Kb dovrebbero essere abbastanza per chiunque. -Bill Gates",
-                                        "I bravi programmatori sanno cosa scrivere. I migliori sanno cosa riscrivere.",
-
-                                       )
-        battuta_scelta_tecnologia = tecnologia_batutte[Math.floor( Math.random() * tecnologia_batutte.length )];
-      $('#materia_testo').text(battuta_scelta_tecnologia);
-    }
-
-  if (materia != "matematica" && materia != "ingenieria" && materia != "scienza" && materia != "tecnologia") {
-    $('#materia_testo').text("Mmmm... non esiste questa materia!😕 ");
-
-  }
-
-  });
-
-
-  // quiz iniviale del nome
+//  Parte iniziale del quiz, chiede il nome della persona e lo salva nella variabile
   $('#invia_nome').click(function(){
     // salva nella variabile nome il nome
     var nome = $("#nome").val();
@@ -88,7 +38,6 @@
       $("#invia_nome").addClass('button_border');
       $("#invia_nome").off()
       }
-
       // se il nome viene inserito allora si scrive sulla textbot mostra aggiungendo un punto esclamativo e faccina
       else {
         $('#mostra').text("ciao, ");
@@ -96,7 +45,7 @@
         $('#mostra').append("! 😊");
         $("#invia_nome").off()
       }
-//avvia un timer da 1 s
+//avvia un timer da 1 s affinchè non ci siano bug tra le transazioni
       setTimeout(
   function()
   {
@@ -118,22 +67,18 @@ function()
       $('#anni_testo').fadeIn(1500)
       $('#invia_anni').fadeIn(1500)
 }, 2000);
-
   });
-
-
 // fine dello javascrit che chiede il nome
 
-//inizo dello js che chiede gli anni
+
+
+//Inizio dello javascript che chiede gli anni
 
   $("#invia_anni").click(function() {
 
-
-
-        //salva nella variabile anni il testo inserito
-        var anni = $("#anni").val();
-
-
+  //salva nella variabile anni il testo inserito
+  var anni = $("#anni").val();
+// controlla se i caratteri sono da a > z senza numeri 
   if(anni.match(/^\d+$/)) {
         //se il testo non contiene niente
          if (anni == "") {
@@ -147,15 +92,14 @@ function()
             $("#invia_anni").removeClass('animated shake');
              });
           }
-
           // se il testo e' uguale a qualcosa allora
           else {
-            // dai due ai 4 anni
+            // da 1  ai 3 anni
             if (anni>=1 && anni<=3) {
               //transazione che fa scomparire i vari input e testi
               $("#invia_anni").fadeOut(500)
               $("#anni").fadeOut(500)
-              //timer che fa andare via gli elementi
+              //timer che fa andare via gli elementi purchè non ci siano bug grafici
               setTimeout(
             function()
             {
@@ -173,7 +117,7 @@ function()
             }
 
 
-
+            // dai 4 agli 8 anni LEGO
             if (anni>=4 && anni<=8) {
                //se anni == 14
               $("#invia_anni").fadeOut(500)
@@ -197,7 +141,7 @@ function()
             }
 
 
-
+            // Dai 9 ai 13 anni LEGO MINDSTORMS
             if (anni>=9 && anni<=13) {
               $("#invia_anni").fadeOut(500)
               $("#anni").fadeOut(500)
@@ -237,7 +181,7 @@ function()
             }, 500);
 
             }
-
+            //dai 14 ai 60 anni  ARDUINO
             if (anni>14 && anni<60) {
                //se anni == 14
               $("#invia_anni").fadeOut(500)
@@ -258,7 +202,7 @@ function()
             }, 500);
 
             }
-//se si inserisce data fasulla
+            //se si inserisce data fasulla
             if (anni>=100) {
               $("#anni_testo").text("Sei troppo vecchio! 😂")
               $("#invia_anni").addClass('animated shake');
@@ -267,16 +211,16 @@ function()
              $("#invia_anni").removeClass('animated shake');
               });
             }
-
+            // più di 150 anni
             if (anni>=150) {
-              $("#anni_testo").text("😂 Credo tu abbia sbagliato!")
+              $("#anni_testo").text("Credo tu abbia sbagliato!")
               $("#invia_anni").addClass('animated shake');
               $("#invia_anni").addClass('button_border');
               $('#invia_anni').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
              $("#invia_anni").removeClass('animated shake');
               });
             }
-//se non sono presenti giochi adatti
+            //se non sono presenti giochi adatti
             if (anni >= 60 && anni<100) {
               $("#anni_testo").text("Non abbiamo giochi adatti per te! 😞")
               $("#invia_anni").addClass('animated shake');
@@ -284,37 +228,93 @@ function()
               $('#invia_anni').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
              $("#invia_anni").removeClass('animated shake');
               });
-
             }
           }
-      }
+       }
 
       else {
+            // se vengono inseriti caratteri speciali, numeri etc
              $('#anni_testo').text("Puoi usare solo caratteri numerici per indicare un'età! 😉");
       }
   });
 
 
+// form nella pagina contatti
+
   $("#invia_contatti").click(function() {
     var problema = $("#contatti_messaggio").val()
-
+    // per ora è solo presente la risposta non abbiamo ancora registratodss
       $("#contatti_testo").text("Non abbiamo ancora registarto questo problema! Contattaci 😃 ")
-
 });
-// INIZIO NAVBAR
 
 
-// animazioni della nav bar
+
+
+
+
+// Quiz Materia
+  $("#invia_materia").click(function() {
+    var materia = $("#materia").val().toLowerCase()
+    // MATEMATICA
+    if (materia == "matematica") {
+      var battue_matematica = new Array("Chi non conosce la matematica difficilmente riesce a cogliere la bellezza, la più intima bellezza, della natura.",
+                                        "Sono persuaso che la matematica sia il più importante strumento di conoscenza fra quelli lasciatici in eredità dall’agire umano, essendo la fonte di tutte le cose.",
+                                        "Come si dice bellezza poetica si dovrebbe dire altresì dire bellezza matematica. -Blaise Pascal" )
+        battuta_scelta_matematica = battue_matematica[Math.floor( Math.random() * battue_matematica.length )];
+      $('#materia_testo').text(battuta_scelta_matematica);
+    }
+    // INGENIERIA
+    if (materia == "ingenieria") {
+      var ingenieria_batutte = new Array("La scienza è sapere; l’ingegneria è fare.",
+                                        "Saranno anche i guerrieri a conquistare la gloria, ma sono gli ingegneri che costruiscono la società.",
+                                        "Una volta sono riuscito a comprendere cosa diceva un ingegnere, ma poi per una settimana non capivo più cosa diceva la gente normale.",
+                                        "Lo scienziato descrive ciò che esiste; l’ingegnere crea ciò che non era mai stato.",
+
+                                       )
+        battuta_scelta_ingenieria = ingenieria_batutte[Math.floor( Math.random() * ingenieria_batutte.length )];
+      $('#materia_testo').text(battuta_scelta_ingenieria);
+    }
+    //SCIENZA
+    if (materia == "scienza") {
+      var scienza_batutte = new Array("La scienza non è che la spiegazione di un miracolo che non riusciamo mai a spiegare e l’arte è un’interpretazione di quel miracolo..",
+                                        "La Natura compone alcune delle sue poesie più belle davanti al microscopio e al telescopio.",
+                                        "La più bella e profonda emozione che possiamo provare è il senso del mistero; sta qui il seme di ogni arte, di ogni vera scienza.",
+                                        "Niente nella vita va temuto, dev’essere solamente compreso. Ora è tempo di comprendere di più, così possiamo temere di meno.",
+
+                                       )
+        battuta_scelta_scienza = scienza_batutte[Math.floor( Math.random() * scienza_batutte.length )];
+      $('#materia_testo').text(battuta_scelta_scienza);
+    }
+    //TECNOLOGIA
+    if (materia == "tecnologia") {
+      var tecnologia_batutte = new Array("I computer sono veloci e stupidi. Gli uomini sono lenti e intelligenti. L'insieme dei due costituisce una forza incalcolabile. ",
+                                        "Il computer è la bicicletta della nostra mente. -Steve Jobs.",
+                                        "640 Kb dovrebbero essere abbastanza per chiunque. -Bill Gates",
+                                        "I bravi programmatori sanno cosa scrivere. I migliori sanno cosa riscrivere.")
+        battuta_scelta_tecnologia = tecnologia_batutte[Math.floor( Math.random() * tecnologia_batutte.length )];
+      $('#materia_testo').text(battuta_scelta_tecnologia);
+    }
+  if (materia != "matematica" && materia != "ingenieria" && materia != "scienza" && materia != "tecnologia") {
+    $('#materia_testo').text("Mmmm... non esiste questa materia!😕 ");
+
+  }
+  });
+
+
+// Inizio NavBar versione da telefono
 
 $("#a_premuto_navbar").click(function(){
-
- $(".navbar_mobile").hide()
+$(".navbar_mobile").hide()
 $("#bottone_mobile").addClass('animated bounceInUp')
-
 });
-//apertura navbar
+
+
+
+// Apertura della Navugation Bar da mobile
 $('#nav_bar_mobile_button').click(function(){
-   $(".spiegazione_div").addClass("display_nothing")
+ 
+
+ $(".spiegazione_div").addClass("display_nothing")
  $("#contatti_testo").addClass("display_nothing")
  $("#contatti_messaggio").addClass("display_nothing")
  $("#invia_contatti").addClass("display_nothing")
@@ -341,17 +341,15 @@ $('#nav_bar_mobile_button').click(function(){
  $(".slider").addClass("display_nothing")
  $(".bx-wrapper").addClass("display_nothing")
  $("#clementoni").addClass("display_nothing")
-$(".youtube_player").addClass("display_nothing")
-
-
-
+ $(".youtube_player").addClass("display_nothing")
 });
 
 
 //animazione di quando si preme il bottone per chiudere la navbar da mobile
 $("#close_icon").click(function() {
-     $(".spiegazione_div").removeClass("display_nothing")
-        $(".spiegazione_div").addClass("animated bounceInUp")
+  
+  $(".spiegazione_div").removeClass("display_nothing")
+  $(".spiegazione_div").addClass("animated bounceInUp")
   $("#contatti_testo").removeClass("display_nothing")
   $("#contatti_messaggio").removeClass("display_nothing")
   $("#invia_contatti").removeClass("display_nothing")
@@ -365,54 +363,36 @@ $("#close_icon").click(function() {
   $("#materia").removeClass("animated bounceInUp")
   $("#invia_materia").addClass("animated bounceInUp")
   $("#materia_testo").addClass("animated bounceInUp")
-$(".testo").removeClass("display_nothing")
-$("#invia_anni").removeClass("display_nothing")
+  $(".testo").removeClass("display_nothing")
+  $("#invia_anni").removeClass("display_nothing")
   $(".youtube_player").removeClass("display_nothing")
-$(".body_centrale").removeClass("display_nothing")
-$(".body_centrale").addClass('animated bounceInUp')
-$("#bottone_mobile").show()
-$("#bottone_mobile style").remove()
-$("#bottone_mobile").addClass("mobile_open")
-$("#bottone_mobile").addClass('animated bounceInUp')
-$("#bottone_mobile").css("style", "")
-$(".navbar_mobile").hide()
-$(".navbar_mobile").removeClass('animated bounce')
-$("footer").removeClass("display_nothing")
-$("footer").addClass('animated bounceInUp')
-$("#invia_nome").removeClass("display_nothing")
-$("#invia_nome").addClass('animated bounceInUp')
-$("#mostra").removeClass("display_nothing")
-$("#mostra").addClass('animated bounceInUp')
-$("#nome").removeClass("display_nothing")
-$("#nome").addClass('animated bounceInUp')
-$("header").removeClass("header_mobile")
-$(".slider").removeClass("display_nothing")
-$(".bx-wrapper").removeClass("display_nothing")
-$("#clementoni").removeClass("display_nothing")
-
-$("header").addClass('animated bounceInUp')
-$(".testo").addClass("animated bounceInUp")
- $(".grey_div").removeClass("display_nothing")
- $(".grey_div").addClass("animated bounceInUp")
-$('.button_mobile').removeAttr('id','button_close');
-$("#close_icon").hide()
-$("#nav_bar_mobile_button").removeClass("display_nothing")
-$('#nav_bar_mobile_button').fadeIn(1000)
-
-
-});
-
-
-
-
-//slider
-
-
-$("#freccia_destra").click(function() {
-
-  $("#img_slider_1").hide()
-  $("#img_slider_2").show()
-
-
-
+  $(".body_centrale").removeClass("display_nothing")
+  $(".body_centrale").addClass('animated bounceInUp')
+  $("#bottone_mobile").show()
+  $("#bottone_mobile style").remove()
+  $("#bottone_mobile").addClass("mobile_open")
+  $("#bottone_mobile").addClass('animated bounceInUp')
+  $("#bottone_mobile").css("style", "")
+  $(".navbar_mobile").hide()
+  $(".navbar_mobile").removeClass('animated bounce')
+  $("footer").removeClass("display_nothing")
+  $("footer").addClass('animated bounceInUp')
+  $("#invia_nome").removeClass("display_nothing")
+  $("#invia_nome").addClass('animated bounceInUp')
+  $("#mostra").removeClass("display_nothing")
+  $("#mostra").addClass('animated bounceInUp')
+  $("#nome").removeClass("display_nothing")
+  $("#nome").addClass('animated bounceInUp')
+  $("header").removeClass("header_mobile")
+  $(".slider").removeClass("display_nothing")
+  $(".bx-wrapper").removeClass("display_nothing")
+  $("#clementoni").removeClass("display_nothing")
+  $("header").addClass('animated bounceInUp')
+  $(".testo").addClass("animated bounceInUp")
+  $(".grey_div").removeClass("display_nothing")
+  $(".grey_div").addClass("animated bounceInUp")
+  $('.button_mobile').removeAttr('id','button_close');
+  $("#close_icon").hide()
+  $("#nav_bar_mobile_button").removeClass("display_nothing")
+  $('#nav_bar_mobile_button').fadeIn(1000)
 });
